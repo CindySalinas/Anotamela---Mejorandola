cdb28768ac270fa5607d1063e87f694f1a9f079c
Cindy Salinas <cindysalinas15@gmail.com>
2015-06-08 21:19:38 -0430

    nuevo modulo

--- server.js
+++ server.js
@@ -8,46 +8,24 @@ var bodyParser = require('body-parser');
 /**
  * Locals
  */
-var app = module.exports = express();
+var server = module.exports = express();
 var port = process.env.PORT || 4000;
-var db = {};
+
 
 // parse json requests
-app.use(bodyParser.json('application/json'));
+server.use(bodyParser.json('application/json'));
 
+var notas = require('./lib/notas');
+server.use(notas);
 /**
  * Routes
  */
-app.post('/notas', function(req, res) {
-  logger.info('POST', req.body);
 
-  // manipulate request
-  var notaNueva = req.body.nota;
-  notaNueva.id = Date.now();
-  db[notaNueva.id] = notaNueva;
-  // prepare response
-  res.set('Content-Type','application/json');
-  res.status(201);
-
-  // send response
-  res.json({
-    nota: notaNueva
-  });
-});
-
-app.get('/notas/:id?', function(req, res){
-  console.log('GET /notas/%s', req.params.id);
-  var id = req.params.id;
-  var nota = db[id];
-  res.json({
-    notas: nota
-  });
-});
 /**
  * Start server if we're not someone else's dependency
  */
 if (!module.parent) {
-  app.listen(port, function() {
+  server.listen(port, function() {
     logger.info('Anotamela API Básico escuchando en http://localhost:%s/', port);
   });
 }
